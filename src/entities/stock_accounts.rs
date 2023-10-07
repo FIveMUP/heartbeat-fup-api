88a71545d3b3bfdafe11a39ea2b75e6a50ffd092
apolo-sys <cristian124421@gmail.com>
2023-10-07 06:28:34 +0200

    chrono impl on db type stock_accounts

--- src/entities/stock_accounts.rs
+++ src/entities/stock_accounts.rs
@@ -1,10 +1,11 @@
 use sqlx::FromRow;
+use chrono::{DateTime, Local};
 
 #[derive(Debug, FromRow)]
 pub struct StockAccount {
     pub id: Option<String>,
     pub owner: Option<String>,
-    pub expireOn: Option<String>,
+    pub expireOn: Option<DateTime<Local>>,
     // pub assignedServer: Option<String>,
     // pub expireOn: Option,
     pub entitlementId: Option<String>,
