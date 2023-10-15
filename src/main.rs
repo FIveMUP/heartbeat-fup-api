3bf1c6515d83021e9ee2e575b7ae40be46780134
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-10-15 21:10:44 +0200

    Implemented graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use config::{init_tracing, Database};
 use dotenvy::dotenv;
 use mimalloc::MiMalloc;
 use std::{net::TcpListener, sync::Arc};
+use tracing::info;
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
@@ -22,12 +23,16 @@ async fn main() {
     dotenv().ok();
     init_tracing();
     let db = Arc::new(Database::new().await);
-
     let listener = TcpListener::bind("0.0.0.0:9000").unwrap();
+    let ctrl_c = tokio::signal::ctrl_c();
 
     Server::from_tcp(listener)
         .unwrap()
         .serve(routes::routes(db).await)
+        .with_graceful_shutdown(async {
+            ctrl_c.await.unwrap();
+            info!("Shutting down server")
+        })
         .await
         .unwrap();
 }
