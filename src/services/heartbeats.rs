a558b071a9c65b9dfed73148c721042a33da2f0a
apolo-sys <cristian124421@gmail.com>
2023-12-22 22:05:16 +0100

    Update heartbeats.rs

--- src/services/heartbeats.rs
+++ src/services/heartbeats.rs
@@ -17,14 +17,14 @@ impl HeartbeatService {
         Self {
             req_client_with_proxy: Client::builder()
                 .proxy(Proxy::all(PROXY_URL).unwrap())
-                .user_agent("CitizenFX/1 (with adhesive; rel. 6798)")
+                .user_agent("CitizenFX/1 (with adhesive; rel. 7194)")
                 .timeout(Duration::from_secs(15))
                 .build()
                 .unwrap(),
 
             req_client_without_proxy: Client::builder()
                 .timeout(Duration::from_secs(15))
-                .user_agent("CitizenFX/1 (with adhesive; rel. 6798)")
+                .user_agent("CitizenFX/1 (with adhesive; rel. 7194)")
                 .build()
                 .unwrap(),
         }
@@ -34,14 +34,14 @@ impl HeartbeatService {
         &self,
         machine_hash: &str,
         entitlement_id: &str,
-        account_index: &str,
+        account_index: &str
     ) -> AppResult<()> {
         let response = {
             let entitlement_heartbeat = format!(
-                "entitlementId={}&f=%7b%7d&gameName=gta5&h2=YyMyxwNpROOEdyxjBu%2bNls1LHzPzx1zTEX7RtDmwD5Eb2MPVgeWNFbNZC3YfGgUnbriTU2jsl7jO0SQ9%2bmDqmU1rLf075r4bxMuKLjcUu2IPy3zVXd2ni2xVJJw8%2bFOoWqaTKIQGggBYEBEBRNOsFNjp6TLqbCwKiqMmc7rl8pLj6SCUm1MpNcBg%2fIE15VmMk4erFf26PdrA4GpAKAP%2fdsM9QaY1GbBnwM4V4xWl8EtLWFPF0XW9xePpm5ZPOjU3OfMAZ2eTF6cNkNsxAGHIMB4VTaKLGWoWmRToEEzbh9wTebY97mYeFdtqF8L%2bnNPVv6y0k4szAwdbInJ2oE73iFj5mZIKLGxqKtNGg9r10nJm2Bk1bTchSWTKlsI%2ffN1vvG6g1fxNDf5%2bJyqGnhktaEMt7L8JTxpgHPuAKtAN795kAM%2fZRgHUUqJzxnH4Ps3jSaMAt5eDpzfdkGvhADFIMMfSEEZ6WqQyvwRw85arnc6IgNYKFlqzGnpsHcWE13elDaRPbgNfMwT7U4Jk31vcfSsadYeqN6Ngad6CeF9zty7GWMklfWcRuaRqtiJvPI3%2fhGymZwPdFHsWvsBEFcbKTWVukjVzaXbuuOH81iY%2fCw7Mbq9A%2f%2fERGFNFW5HXUd9WCZsUooXHJcjVuczxO0BgQLfyEGaaemQSr0RwA3abTe7l5nY4wMC%2fJKkB1AKURTTsJcHhbK0Xrz14b5XOZIZDNlUGQpXweFTMWeualdOAxGUvDnnD0%2fqIZ39zjnPdulZUxCzGt%2fPt1Mt2nsAEJaYq%2fSLBqoahs9UtgGs%2fX9PAqqsnJdsRJ%2bZXKA%2fGfeBr58TCQsDJ8B1CCkqqsmAjItskmOY6w2%2fNGhQw7enImzXwvO4%3d&machineHash=AQAL&machineHashIndex={}&rosId=1234&i={}",
+                "entitlementId={}&f=%7b%7d&gameName=gta5&h2=YyMyxwNpROOEdyxjBu%2bNls1LHzPzx1zTEX7RtDmwD5Eb2MPVgeWNFbNZC3YfGgUnbriTU2jsl7jO0SQ9%2bmDqmU1rLf075r4bxMuKLjcUu2IPy3zVXd2ni2xVJJw8%2bFOoWqaTKIQGggBYEBEBRNOsFNjp6TLqbCwKiqMmc7rl8pLj6SCUm1MpNcBg%2fIE15VmMk4erFf26PdrA4GpAKAP%2fdsM9QaY1GbBnwM4V4xWl8EtLWFPF0XW9xePpm5ZPOjU3OfMAZ2eTF6cNkNsxAGHIMB4VTaKLGWoWmRToEEzbh9wTebY97mYeFdtqF8L%2bnNPVv6y0k4szAwdbInJ2oE73iFj5mZIKLGxqKtNGg9r10nJm2Bk1bTchSWTKlsI%2ffN1vvG6g1fxNDf5%2bJyqGnhktaEMt7L8JTxpgHPuAKtAN795kAM%2fZRgHUUqJzxnH4Ps3jSaMAt5eDpzfdkGvhADFIMMfSEEZ6WqQyvwRw85arnc6IgNYKFlqzGnpsHcWE13elDaRPbgNfMwT7U4Jk31vcfSsadYeqN6Ngad6CeF9zty7GWMklfWcRuaRqtiJvPI3%2fhGymZwPdFHsWvsBEFcbKTWVukjVzaXbuuOH81iY%2fCw7Mbq9A%2f%2fERGFNFW5HXUd9WCZsUooXHJcjVuczxO0BgQLfyEGaaemQSr0RwA3abTe7l5nY4wMC%2fJKkB1AKURTTsJcHhbK0Xrz14b5XOZIZDNlUGQpXweFTMWeualdOAxGUvDnnD0%2fqIZ39zjnPdulZUxCzGt%2fPt1Mt2nsAEJaYq%2fSLBqoahs9UtgGs%2fX9PAqqsnJdsRJ%2bZXKA%2fGfeBr58TCQsDJ8B1CCkqqsmAjItskmOY6w2%2fNGhQw7enImzXwvO4%3d&i={}&machineHash=AQAL&machineHashIndex={}&rosId=1234",
                 entitlement_id,
-                urlencoding::encode(machine_hash),
-                account_index
+                account_index,
+                machine_hash
             );
 
             self.req_client_with_proxy
@@ -60,6 +60,7 @@ impl HeartbeatService {
         if response.is_success() {
             Ok(())
         } else {
+            println!("Entitlement heartbeat failed: {}", response);
             Err(CfxApiError::StatusCodeNot200)?
         }
     }
@@ -74,7 +75,7 @@ impl HeartbeatService {
         let response = {
             let ticket_heartbeat = format!(
                 "gameName=gta5&guid=148618792012444134&machineHash=AQAL&machineHashIndex={}&server=http%3a%2f%51.91.102.108%3a30120%2f&serverKeyToken={}&token={}&i={}",
-                urlencoding::encode(machine_hash),
+                machine_hash,
                 urlencoding::encode(sv_license_key_token),
                 entitlement_id,
                 account_index
